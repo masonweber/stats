f9f9591179fdd6df1d26c62a06844757ec76c814
Mason Weber <mason.weber.cpe@gmail.com>
2016-03-29 16:44:02 -0600

    project upload

---
+++ src/mweb/stats/Normal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mweb.stats;
+
+import java.util.Set;
+
+/**
+ *
+ * @author Mason
+ */
+public class Normal {
+    //class vars
+    private double mean, variance, stdDev;
+    //constructor
+    public Normal(Set s) {
+        
+    }
+    //methods
+    private double mean() {
+        
+        return 0.0;
+    }
+    
+    private double variance() {
+        //take mean
+        
+        //sum (x-u)^2
+        
+        //divide by n-1
+        return 0.0;
+    }
+    
+    private double standardDeviation() {
+        //square root of variance;
+        return 0.0;
+    }
+}
---
+++ src/mweb/stats/Stats.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mweb.stats;
+
+/**
+ *
+ * @author Mason
+ */
+public class Stats {
+    //t test
+    //anova
+    //lookup table
+    //confidence intervals
+    //chi squared test
+    //linear regression
+    //binary logistic regression
+    //sample runs
+    //distributions
+}
